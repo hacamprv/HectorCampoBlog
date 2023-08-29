694990726e14dbd6d64a9c98c35dcc5ccd30afde
Hector Campo <hiddencsi.c789zv@gmail.com>
2023-08-28 23:17:14 -0400

    Changed the let to const and made html and css kabob case

--- Main/main.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:08c462abdcc6d9ce26b56fd9fd124efd8ab4316b73aca1010f2f544572dc2515
-size 3636
---
+++ main.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fac6529d01f2f072ac725f9cba46fc4aa17f1ab2cafe5d4f3760bd7df1825c9c
+size 3726
