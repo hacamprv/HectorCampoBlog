433685762be11c20c4227c4bcc107c954363c242
Hector Campo <hiddencsi.c789zv@gmail.com>
2023-08-31 22:04:37 -0400

    added parameters to the javascript

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fac6529d01f2f072ac725f9cba46fc4aa17f1ab2cafe5d4f3760bd7df1825c9c
-size 3726
+oid sha256:6daa7b80fc0da5ece975160be9abeea67847387c87ca1cecbf83f37dbbff1199
+size 2417
