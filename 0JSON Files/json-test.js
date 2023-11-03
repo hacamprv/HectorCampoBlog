1259c714a9a82cd3c84c8f6bd51a368d140c66e6
Hector Campo <hiddencsi.c789zv@gmail.com>
2023-11-02 20:00:43 -0400

    expirementing with node and JSON

--- 0JSON Files/json-test.js
+++ 0JSON Files/json-test.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85ed3da9b8128d9b3e7d6b0d079542b9bcf256ae9172ceae083b3f81a8bbd25d
-size 324
+oid sha256:ebec0c18cd1059342e81cf8b51bbdc6cc478c0602891ee81c14433b037921a61
+size 366
---
+++ NodeServer/app.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f63b5bf097e45aebb127e3d37a1cd14cd536597ee77d3d68f1b23b7659b42ab0
+size 1428
