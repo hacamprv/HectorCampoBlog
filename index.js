d6d5af0f92e0ca30dd353e099d6e9be7aba16e06
Hector Campo <hiddencsi.c789zv@gmail.com>
2024-01-13 18:16:05 -0500

    deleted extra CSS files and made minor coding changes

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:00d00be3cea92e3191ad52cab18355e1ea9b22b8cffba3fac773463bbe215f95
-size 5339
+oid sha256:8cea863514e19cf86cae4fa36230f14d9de49e01854b9cf0ab1f26f6d79a9678
+size 5392
