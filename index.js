322955e97cefd73d6a2085c1ea5e7280275575d2
Hector Campo <hiddencsi.c789zv@gmail.com>
2024-03-04 23:15:33 -0500

    added switches to switch between photos

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2d64e694d17ca9123522fca1104e6d56954043ed0415c3076ceb8e4107a10842
-size 4399
+oid sha256:d195d4e8c39bba7986a2c4df1f2d40c2f8dc84579f55dc782135edc19ef32b3a
+size 6322
