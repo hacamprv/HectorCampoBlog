3994c21ed93f6e161f55d5fb6b096abdabf9306f
Hector Campo <hiddencsi.c789zv@gmail.com>
2024-05-27 22:47:59 -0400

    made html lang en, updated the template correctly adding head elements and body elements, and added a favicon

---
+++ Blog JS/loader.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d5e59c9fe43f273e28f1b0a819df821bef59932d1d16cdf9be64fc42dba1071a
+size 538
--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43a13c1fde00c812a398c1801f5a0ea3b2ebe0f6ff5b463f25cf0f89b757e34a
-size 1976
+oid sha256:c5c8a2b05511b3653fe035193946b555f7e9a2703e7e880603868348c7802d69
+size 2535
